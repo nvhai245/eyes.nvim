263736a5971ea046bc0b027ee9069ab569275cdb
Hai Nguyen <nvhai245@gmail.com>
2025-05-15 22:12:25 +0700

    brighter color

--- colors/eyes.lua
+++ colors/eyes.lua
@@ -3,17 +3,17 @@ local palette = nil
 
 if vim.o.background == "dark" then
 	palette = {
-		hex00 = "#000000",
-		hex01 = "#101010",
-		hex02 = "#202020",
-		hex03 = "#303030",
-		hex04 = "#404040",
-		hex05 = "#505050",
-		hex06 = "#606060",
-		hex07 = "#707070",
-		hex08 = "#808080",
-		hex09 = "#909090",
-		hex10 = "#A0A0A0",
+		hex00 = "#303030",
+		hex01 = "#404040",
+		hex02 = "#505050",
+		hex03 = "#606060",
+		hex04 = "#707070",
+		hex05 = "#808080",
+		hex06 = "#909090",
+		hex07 = "#A0A0A0",
+		hex08 = "#B0B0B0",
+		hex09 = "#C0C0C0",
+		hex10 = "#D0D0D0",
 	}
 else
 	palette = {
